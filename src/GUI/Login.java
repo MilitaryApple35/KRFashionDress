fc57b9984116cd1a53cc97ed3bacfcd0956c2d0e
MilitaryApple35 <pabloademir05@gmail.com>
2023-06-28 08:40:06 -0700

    cambios a login

--- src/GUI/Login.java
+++ src/GUI/Login.java
@@ -111,7 +111,7 @@ public class Login extends javax.swing.JFrame {
                 .addComponent(jLabel3)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(pssContrasenia, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 40, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(btnSIG)
                 .addGap(32, 32, 32)
                 .addComponent(btnSalir1)
